ce6c62beb9779b8fcb039f74f4f22b9eaa06974b
dcunhamaciel <dcunhamaciel@gmail.com>
2023-01-28 11:32:09 -0300

    Controller Post

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('posts', [App\Http\Controllers\PostController::class, 'get']);
+Route::post('posts', [App\Http\Controllers\PostController::class, 'store']);
+Route::delete('posts/{id}', [App\Http\Controllers\PostController::class, 'delete']);

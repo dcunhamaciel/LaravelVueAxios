2ff22cb433dafc0c9b01aecfa5414aaa6faf336c
dcunhamaciel <dcunhamaciel@gmail.com>
2023-01-28 11:57:10 -0300

    Gerenciamento de Estado pelo Vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-import './bootstrap';
+require('./bootstrap');
+window.Vue = require('vue');
+
+import store from './store/index';
+
+Vue.component('posts', require('./components/Posts.vue'));
+Vue.component('createPost', require('./components/CreatePost.vue'));
+
+const app = new Vue({
+    el: '#app',
+    store
+});
